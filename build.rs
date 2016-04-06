0004bafe5fed57272130f12fa9060592a3b460a6
David Irvine <david.irvine@maidsafe.net>
2016-04-06 19:22:54 +0100

    Do not run build.rs on windows for now
    Windows requires openssl supposrt and this is currently not part of CI

--- build.rs
+++ build.rs
@@ -32,39 +32,44 @@ use std::fs::{self, File};
 use std::io;
 use std::path::{Path, PathBuf};
 
+// FIXE openssl on windows is required
+#[cfg(not(target_os = "windows"))]
 fn main() {
-  // Only generate the diagrams when "generate-diagrams" feature is enabled.
-  // TODO: instead of this feature, detect that cargo is run in the "doc"
-  // profile.
-  if env::var("CARGO_FEATURE_GENERATE_DIAGRAMS").is_err() {
-    return;
-  }
+    // Only generate the diagrams when "generate-diagrams" feature is enabled.
+    // TODO: instead of this feature, detect that cargo is run in the "doc"
+    // profile.
+    if env::var("CARGO_FEATURE_GENERATE_DIAGRAMS").is_err() {
+        return;
+    }
 
-  // List all diagram names and URLs to download them from.
-  download_image("personas",                "https://cacoo.com/diagrams/wl6of3FUFriB0FWO-0BD19.png");
-  download_image("immutable-data-put-flow", "https://cacoo.com/diagrams/SCHrwEhLRB86EGe1-EF9A0.png");
-  download_image("immutable-data-get-flow", "https://cacoo.com/diagrams/ndcPMKC3WapABSaA-EF9A0.png");
+    // List all diagram names and URLs to download them from.
+    download_image("personas",
+                   "https://cacoo.com/diagrams/wl6of3FUFriB0FWO-0BD19.png");
+    download_image("immutable-data-put-flow",
+                   "https://cacoo.com/diagrams/SCHrwEhLRB86EGe1-EF9A0.png");
+    download_image("immutable-data-get-flow",
+                   "https://cacoo.com/diagrams/ndcPMKC3WapABSaA-EF9A0.png");
 }
 
 fn download_image<U: IntoUrl>(name: &str, src: U) {
-  download(src, image_path(name))
+    download(src, image_path(name))
 }
 
 fn download<U: IntoUrl, P: AsRef<Path>>(src: U, dst: P) {
-  let client = Client::new();
-  let mut res = client.get(src).send().unwrap();
+    let client = Client::new();
+    let mut res = client.get(src).send().unwrap();
 
-  if let Some(dir) = dst.as_ref().parent() {
-    fs::create_dir_all(dir).unwrap();
-  }
+    if let Some(dir) = dst.as_ref().parent() {
+        fs::create_dir_all(dir).unwrap();
+    }
 
-  let mut file = File::create(dst).unwrap();
-  io::copy(&mut res, &mut file).unwrap();
+    let mut file = File::create(dst).unwrap();
+    io::copy(&mut res, &mut file).unwrap();
 }
 
 fn image_path(name: &str) -> PathBuf {
-  let mut path = PathBuf::from("target/doc/safe_vault");
-  path.push(name);
-  path.set_extension("png");
-  path
+    let mut path = PathBuf::from("target/doc/safe_vault");
+    path.push(name);
+    path.set_extension("png");
+    path
 }
