68e17812e981c363cd8b0661b5104020d8dc1285
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-05-02 13:24:16 +0100

    style/test suite: relocate test and fix typos
    This moves tests which depend on mock Crust to the test folder.

--- src/lib.rs
+++ src/lib.rs
@@ -36,10 +36,10 @@
 #![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
         non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
         private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
-        unknown_lints, unsafe_code, unused_allocation, unused_attributes,
+        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
         unused_comparisons, unused_features, unused_parens, while_true)]
-#![warn(trivial_casts, trivial_numeric_casts, unused_import_braces,
-        unused_qualifications, unused, unused_extern_crates, unused_results)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
 #![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
          missing_debug_implementations, variant_size_differences)]
 
