241571b65975c763373fa8e4e3c31b30cbba3b2a
bochaco <gabrielviganotti@gmail.com>
2020-08-18 19:10:25 -0500

    refactor(node): adapt code to make use of routing async API

--- src/lib.rs
+++ src/lib.rs
@@ -44,5 +44,5 @@ pub use crate::{
     config_handler::{write_connection_info, Config},
     error::{Error, Result},
     network::Network,
-    node::{state_db::Command, Node, Receiver},
+    node::{state_db::Command, Node},
 };
